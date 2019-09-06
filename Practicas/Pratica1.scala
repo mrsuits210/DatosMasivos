7edc410042008982bfdd6f12147372b8c69eafdd
Luis Adrian garcia <luis.garciamub@gmail.com>
2019-09-05 23:25:53 -0700

    3 y 4

--- Practicas/Pratica1.scala
+++ Practicas/Pratica1.scala
@@ -1,1 +1,26 @@
+// Assessment 1/Practica 1
+//1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
+val cir = 15
+val pi = 2*3.1416
+val rad = cir / pi
 
+
+println(rad) 
+
+
+
+
+
+//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+//3. Dada la variable bird = "tweet", utiliza interpolacion de string para
+//   imprimir "Estoy ecribiendo un tweet"
+val bird = "tweet"
+val interpolar = "Estoy escribiendo un "+ bird
+
+
+
+//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
+//   secuencia "Luke"
+    val star = List("Hola","Luke","Yo soy tu padre")
+   
+    star.slilce(2)
