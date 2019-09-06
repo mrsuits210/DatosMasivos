b6f92b79e0dc86fd2b3dfad8ed4b6be22bedbadb
Luis Adrian garcia <luis.garciamub@gmail.com>
2019-09-06 00:02:12 -0700

    Practicas Resueltas

--- Practicas/Pratica1.scala
+++ Practicas/Pratica1.scala
@@ -12,6 +12,8 @@ println(rad)
 
 
 //2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+
+
 //3. Dada la variable bird = "tweet", utiliza interpolacion de string para
 //   imprimir "Estoy ecribiendo un tweet"
 val bird = "tweet"
@@ -21,6 +23,14 @@ val interpolar = "Estoy escribiendo un "+ bird
 
 //4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
 //   secuencia "Luke"
-    val star = List("Hola","Luke","Yo soy tu padre")
-   
-    star.slilce(2)
+  val star = "Hola Luke Yo soy tu padre" 
+    star.slice(5,9)
+
+//5. Cual es la diferencia en value y una variable en scala? 
+// La Variable Puede Cambiar de Valor y se denomina variable Mutable 
+// Value es una variable que no  puede cambiar  de valor y se llama variable inmutable. 
+
+//6. Dada la tupla ((2,4,5),(1,2,3),(3.1416,23))) regresa el numero 3.1416 
+
+val tupla = ((2,4,5),(1,2,3),(3.1416,23))
+println(tupla._3)
