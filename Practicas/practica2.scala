94d9ad255322bf1cd8f2b2789fd15ead5d13ed98
Luis Adrian garcia <luis.garciamub@gmail.com>
2019-10-04 19:21:28 -0700

    Last Update

--- Practicas/practica2.scala
+++ Practicas/practica2.scala
@@ -1,17 +1,18 @@
 // 1. Crea una lista llamada "lista" con los elementos "rojo", "blanco", "negro"
  val lista = List("rojo","blanco","negro")
- println(lista) 
+ println(lista)
 
 // 2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
 val c1 = "Verde" :: lista
-val c2 = "Amarillo" :: c1 
+val c2 = "Amarillo" :: c1
 val c3 = "Azul" :: c2
-val c4 = "Naranja" :: c3 
-val c5 = "Perla" :: c4 
+val c4 = "Naranja" :: c3
+val c5 = "Perla" :: c4
 
 // 3. Traer los elementos de "lista" "verde", "amarillo", "azul"
 
-
+var lista = List("rojo", "blanco", "negro","verde","amarillo","azul", "naranja", "perla")
+lista.slice(3,6)
 // 4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
 
 val array = (1 to 1000).by(5)
@@ -28,9 +29,3 @@ val array = (1 to 1000).by(5)
 //  Imprime todas la llaves del mapa
 val nombres = collection.mutable.Map(("Jose",20),("Luis",24),("Ana",23),("Susana",27))
 nombres.keys
-
-
-
-
-
-
