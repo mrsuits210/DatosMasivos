41c4c63f1b2d47d91d3c650fbaefff15144cd666
Luis Adrian garcia <luis.garciamub@gmail.com>
2019-11-13 19:01:42 -0800

    Last Commit

--- Unidad2/Evaluacion/examen.scala
+++ Unidad2/Evaluacion/examen.scala
@@ -5,57 +5,45 @@ import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import spark.implicits._
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.feature.StringIndexer
-import org.apache.spark.ml.feature.VectorIndexer
-import org.apache.spark.ml.feature.IndexToString
-import org.apache.spark.sql.Dataset._
 
-val Spark= SparkSession.builder().getOrCreate()
+val data = spark.read.format("csv").option("inferSchema","true").option("header","true").csv("iris.csv")
 
-val data = spark.read.format("csv")
-.option("sep",";")
-.option("inferSchema","true")
-.option("header","true")
-.csv("iris.csv")
-
+data.printSchema()
 // identify the feature colunms
-val inputColumns = ("SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm")
-val assembler = new VectorAssembler().setInputCols(inputColumns).setOutputCol("features")
-val featureSet = assembler.transform(data);
+val assembler = new VectorAssembler().setInputCols(Array("sepal_length","sepal_width","petal_length","petal_width")).setOutputCol("features")
+val featureSet = assembler.transform(data)
 
 
-val labelIndexer = new StringIndexer().setInputCol("class").setOutputCol("indexedLabel").fit(featureSet)
-println(s"Found labels: ${labelIndexer.labels.mkString("[", ", ", "]")}")
+val labelIndexer = new StringIndexer().setInputCol("species").setOutputCol("label")
+val dataindex = labelIndexer.fit(featureSet).transform(featureSet)
+
+
+
 
 //Los valores que se indentifican son indexados y se añade maxCategories para que los valores mayores a 4 sean tratados como continuos
-val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(featureSet)
+///val featureIndexer = new VectorIndexer().setInputCol("features").setOutputCol("indexedFeatures").setMaxCategories(4).fit(featureSet)
 
 // split data random in trainingset (70%) and testset (30%)
 
 
 
-val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
+val splits = dataindex.randomSplit(Array(0.6, 0.4), seed = 1234L)
 val train = splits(0)
 val test = splits(1)
 
-val layers = Array[Int](3, 5, 4, 3)
+val layers = Array[Int](4, 5, 4, 3)
 
 
 
-val trainer = new MultilayerPerceptronClassifier()
-  .setLayers(layers)
-  .setBlockSize(128)
-  .setSeed(1234L)
-  .setMaxIter(100)
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100)
 
-  val model = trainer.fit(train)
+val model = trainer.fit(train)
 
 //  Creamos nuestro modelo de computacion para determinar nuestro valor de predccion mas acertado 
 val result = model.transform(test)
 val predictionAndLabels = result.select("prediction", "label")
-val evaluator = new MulticlassClassificationEvaluator()
-  .setMetricName("accuracy")
+val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
 
 println(s"Test set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
-
 
 
